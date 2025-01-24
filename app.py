6e83921ac2e4e1c8c32171823f40aeeefd4ffa26
Shakya <prakritishakya1168@gmail.com>
2025-01-23 21:22:17 -0800

    updated app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.title('Customer Churn Prediction')
 #User Input
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
-age = st.slider('Age', 18-92)
+age = st.slider('Age', 18,92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
 estimated_salary = st.number_input('Estimated Salary')
@@ -33,9 +33,30 @@ num_of_prducts = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0,1])
 is_active_member = st.selectbox('Is Active Member', [0,1])
 
+
 #one hot encoded geography
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded,onehot_encoder_geo.get_feature_names_out(['Geography']))
+
+#prepare the input data
+input_data = pd.DataFrame({
+    'CreditScore' : [credit_score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age' : [age],
+    'Tenure' : [tenure],
+    'Balance' : [balance],
+    'NumOfProducts' : [num_of_prducts],
+    'HasCrCard' : [has_cr_card],
+    'IsActiveMember' : [is_active_member],
+    'EstimatedSalary' :[estimated_salary]
+
+})
+
+#one hot encoded geography
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+# geo_encoded = onehot_encoder_geo.transform([[input_data['Geography']]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded,columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+
 
 #combine one-hot encoded caolumns into input data
 input_data = pd.concat([input_data.reset_index(drop=True),geo_encoded_df],axis=1)
